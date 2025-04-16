08dca130807f4bed0b7aa388e94301195d29c3f6
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-04-16 16:49:20 +0200

    Part 7

---
+++ BroCode_C_Programming_Course/Part7/constants.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+
+    // constant = fixed value that cannot be altered by the program during its execution
+
+    const float PI = 3.14159;
+
+    printf("%f", PI);
+    
+    return 0;
+}
