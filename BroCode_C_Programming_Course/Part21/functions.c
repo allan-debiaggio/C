3c25e4d33e3e72853c1d7d07833ce4455e13dfe6
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-04-22 10:23:51 +0200

    Part 21

---
+++ BroCode_C_Programming_Course/Part21/functions.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+void birthday()
+{
+    printf("\nHappy birthday to you !");
+    printf("\nHappy birthday to you !");
+    printf("\nHappy birthday dear... YOU !");
+    printf("\nHappy birthday to you !");
+}
+
+int main(void)
+{
+    birthday();
+
+    return 0;
+}
