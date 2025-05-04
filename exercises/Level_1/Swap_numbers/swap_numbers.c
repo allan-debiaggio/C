f7f89c728ba3b383421ccf0fdf15aa2055a169bf
allan.debiaggio <allan.de-biaggio@laplateforme.io>
2025-05-04 10:25:24 +0200

    Level 1 Exercise 4 Bonus

--- exercises/Level_1/Swap_numbers/swap_numbers.c
+++ exercises/Level_1/Swap_numbers/swap_numbers.c
@@ -14,7 +14,15 @@ int main (void)
     scanf(" %d", &num2);
     printf("Number 1 = %d, Number 2 = %d\n", num1, num2);
     swapper = num2, num2 = num1, num1 = swapper;
+    printf("Swapped numbers : Number 1 = %d, Number 2 = %d\n", num1, num2);
+
+    // Second part, not using third variable
+    printf("Give me the first integer : ");
+    scanf(" %d", &num2);
+    printf("Give me the second integer : ");
+    scanf(" %d", &num1);
     printf("Swapped numbers : Number 1 = %d, Number 2 = %d", num1, num2);
+
 
     return 0;
 }
